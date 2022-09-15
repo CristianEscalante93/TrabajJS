294a272e28d3b88ab5b2d3d952afa38a800a0b9d
Cristian Escalante <kykanbeca@gmail.com>
2022-09-14 21:10:06 -0300

    agregado de eventos al DOM

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ const carrito = [];
 //constantes
 const h2 = document.getElementById("h2");
 const stockProductos= document.getElementById("stock");
-const compras = document.getElementById("compras")
+const compras = document.getElementById("compras");
+const nomb = document.getElementById("nombre");
+const email = document.getElementById("email");
+const btnEnviar = document.getElementById("btnCargar");
+const precioTotal = document.getElementById("total");
+
 
 // clase constructora de objetos-productos
 class productoStock {
@@ -36,105 +41,77 @@ cargar(stock,nuevoProducto5)
 
 // inicio de pedido de datos al cliente
 let nombre;
-function NombreMail() {
-    alert("Bienvenido a la mas variada tienda de iluminación")
-    nombre = prompt("Ingrese su nombre");
-    while(nombre === "" || !isNaN(nombre)){
-    nombre = prompt("Ingrese su nombre");
-    return}
-    let mail = prompt("Ingrese su mail");
-    while(mail === ""){
-    mail = prompt("Ingrese su mail");
-    }
-    alert("Bienvenido " + nombre + " , espero encuentre lo que esta buscando");
+btnEnviar.addEventListener("click",()=> {
+    nombre = nomb.value;
     h2.innerText= "Buenas " + nombre + " , a continuacion podra ver su compra";
-}
-
-// eleccion de productos
-let seguir = true;
-let insumo;
-function lamparas() {
-    let precioTotal;
-    while (seguir === true) {
-        insumo = parseInt(prompt(("Elija su accesorio de iluminación : \n1)Colgante de 1 cuerpo\n2)Colgante de 2 cuerpos\n3)Lámpara vintage\n4)Lámpara moderna\n5)Solo portalámparas\n6)Finalizar pedido")));
-        switch (insumo) {
-            case 1:
-                cargar(carrito, nuevoProducto1);
-                alert('Selecciono' + ' ' + nuevoProducto1.nombre + ' ' + 'que cuesta $' + ' ' + nuevoProducto1.precio);
-                break;
-            case 2:
-                cargar(carrito, nuevoProducto2);
-                alert('Selecciono' + ' ' + nuevoProducto2.nombre + ' ' + 'que cuesta $' + ' ' + nuevoProducto2.precio);
-                break;
-            case 3:
-                cargar(carrito, nuevoProducto3);
-                alert('Selecciono' + ' ' + nuevoProducto3.nombre + ' ' + 'que cuesta $' + ' ' + nuevoProducto3.precio);
-                break;
-            case 4:
-                cargar(carrito, nuevoProducto4);
-                alert('Selecciono' + ' ' + nuevoProducto4.nombre + ' ' + 'que cuesta $' + ' ' + nuevoProducto4.precio);
-                break;
-            case 5:
-                cargar(carrito, nuevoProducto5);
-                alert('Selecciono' + ' ' + nuevoProducto5.nombre + ' ' + 'que cuesta $' + ' ' + nuevoProducto5.precio);
-                break;
-            case 6:
-                seguir = false;
-                alert('Usted ha finalizado la compra');
-                break;
-            default:
-                alert('Opción no válida');
-                break;}
-                
-            precioTotal = carrito.reduce((acc,el)=> acc + el.precio, 0 );
-            
-            }
-            return precioTotal;
-        }
-
-// pago
-function cobro(precio){
-alert("Precio total de la compra: $"+precio);
-let formaPago;
-do{
-    formaPago = parseInt(prompt("Como desea abonar?\n1)Efectivo\n2)Tarjeta"))
-}while(formaPago < 1 || formaPago > 2)
-let pago;
-if (formaPago==1){
-    pago = parseInt(prompt("con cuanto abona??"))
-    if(pago>precio){
-        alert("Muchas gracias, su cambio es " + (pago-precio))
-    }else if(pago==precio){
-        alert("Abono con cambio justo, por favor pase a retirar su compra, muchas gracias!!")
-    }
-}else if(formaPago==2){
-    alert("Gracias por su compra, revise su mail donde encontrara el link de pago, muchas gracias!!")
-}
-}
+})
 
 // tarjetas del stock
-
+function tarjetasStock () {
 for (const producto of stock) {
     let prod = document.createElement('div')
     prod.innerHTML=`<div class="card">
     <h3>${producto.nombre}</h3>
-    <p>${producto.precio}</p>
+    <p> $ ${producto.precio}</p>
+    <button id="agregar${producto.id}">Agregar</button>
     </div>`
     stockProductos.append(prod) 
 }
+funcionBoton()
+}
 
-NombreMail();
-const resultado = lamparas();
-console.log(resultado);
-cobro(resultado);
+// boton agregar
+function funcionBoton(){
+    for (const producto of stock) {
+        document.getElementById(`agregar${producto.id}`).addEventListener("click",()=>{
+            agregarCarrito(producto)
+    });
+}
+};
 
-// tarjetas de compras
+function agregarCarrito(producto){
+    let existe = carrito.some(prod=>prod.id === producto.id);
+    if(existe===false){
+        producto.cantidad = 1;
+        cargar(carrito,producto);
+    }
+    else{
+        let prodFind = carrito.find(prod=> prod.id===producto.id);
+        prodFind.cantidad++;
+    }
+    mostrarCarrito();
+    }
+
+// mostrar el carrito de compras
+function mostrarCarrito(){
+    compras.innerHTML = "";
 for (const producto of carrito) {
     let prodCarrito = document.createElement('div')
     prodCarrito.innerHTML=`<div class="card">
     <h3>${producto.nombre}</h3>
-    <p>${producto.precio}</p>
+    <p> $ ${producto.precio}</p>
+    <h3>CANTIDAD: ${producto.cantidad}</h3>
+    <button class="btnCarrito" id="btn-borrar${producto.id}">Borrar</button>
     </div>`
     compras.append(prodCarrito) 
 }
-/* <img src="../img/${servicio.img}" alt=""></img> */
+borrar();
+let precioTot;
+    precioTot = carrito.reduce((acc,el)=> acc + ((el.precio)*(el.cantidad)), 0 );
+precioTotal.innerText= "El valor de su compra es de $" + precioTot;
+}
+
+// boton borrar
+function borrar(){
+    carrito.forEach(producto=>{
+        document.getElementById(`btn-borrar${producto.id}`).addEventListener("click",()=>{
+            let indice = carrito.findIndex(element=>element.id===producto.id);
+            carrito.splice(indice,1);
+            mostrarCarrito();
+        })
+    })
+}
+
+
+mostrarCarrito();
+tarjetasStock();
