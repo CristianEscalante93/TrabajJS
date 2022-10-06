2187e1339d5275d2d976750b23e009bd72461e9e
Cristian Escalante <kykanbeca@gmail.com>
2022-10-05 22:34:56 -0300

    agregado de fetch

--- main.js
+++ main.js
@@ -16,32 +16,67 @@ const checkbox = document.getElementById("checkbox");
 const precioTotal = document.querySelector(".total");
 const finalizar = document.getElementById("finalizar");
 const cancelar = document.getElementById("cancelar");
+const cuotas = document.querySelector('#cuotas');
+const formaPago = document.querySelector('#formaPago');
+const formCuota = document.querySelector('.form-cuota');
+const divCuota = document.querySelector('.divCuotas');
+const tipoDePago = document.querySelector('.tipoDePago');
 
 
-// clase constructora de objetos-productos
-class productoStock {
-    constructor(id, nombre, precio) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;}}
+// // clase constructora de objetos-productos
+// class productoStock {
+//     constructor(id, nombre, precio) {
+//     this.id = id;
+//     this.nombre = nombre;
+//     this.precio = precio;}}
 
-// agregado de productos
-const nuevoProducto1 = new productoStock(1,"Colgante de 1 cuerpo",1500);
-const nuevoProducto2 = new productoStock(2,"Colgante de 2 cuerpos",2000);
-const nuevoProducto3 = new productoStock(3,"Lámpara vintage",1000);
-const nuevoProducto4 = new productoStock(4,"Lámpara moderna",2500);
-const nuevoProducto5 = new productoStock(5,"Solo portalamparas",500);
+// // agregado de productos
+// const nuevoProducto1 = new productoStock(1,"Colgante de 1 cuerpo",1500);
+// const nuevoProducto2 = new productoStock(2,"Colgante de 2 cuerpos",2000);
+// const nuevoProducto3 = new productoStock(3,"Lámpara vintage",1000);
+// const nuevoProducto4 = new productoStock(4,"Lámpara moderna",2500);
+// const nuevoProducto5 = new productoStock(5,"Solo portalamparas",500);
 
 // funcion agregar para cualquier array
 function cargar(array,objeto){
     array.push(objeto);
 }
 
-cargar(stock,nuevoProducto1)
-cargar(stock,nuevoProducto2)
-cargar(stock,nuevoProducto3)
-cargar(stock,nuevoProducto4)
-cargar(stock,nuevoProducto5)
+// cargar(stock,nuevoProducto1)
+// cargar(stock,nuevoProducto2)
+// cargar(stock,nuevoProducto3)
+// cargar(stock,nuevoProducto4)
+// cargar(stock,nuevoProducto5)
+
+const stock1 = [];
+//     { id: 1, nombre: "Colgante de 1 cuerpo", precio: 1500, img: "colgante de un cuerpo.jpg" },
+//     { id: 2, nombre: "Colgante de 2 cuerpos", precio: 2000, img: "colgante 2 cuerpos.jpg" },
+//     { id: 3, nombre: "Lámpara vintage", precio: 1000, img: "lampara vintage.jpg" },
+//     { id: 4, nombre: "Lámpara moderna", precio: 2500, img: "lampara moderna.jpg" },
+//     { id: 5, nombre: "Solo portalamparas", precio: 500, img: "solo portalamparas.jpg" },
+//     { id: 6, nombre: "Lámpara globo", precio: 500, img: "lampara globo.jpg" },
+// ];
+console.log(stock1)
+async function fetchAPI(){
+    try {
+    const URL='/data.json';
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+for (const producto of data) {
+    cargar(stock,producto)
+}
+tarjetasStock();
+    } catch (error) {
+    console.log(error);
+    }
+}
+
+fetchAPI()
+
+
+
+
 
 // inicio de pedido de datos al cliente (use operador ternario)
 let nombre;
@@ -102,12 +137,21 @@ getDatos(JSON.parse(localStorage.getItem("cliente")));
 // tarjetas del stock (use destructuring)
 function tarjetasStock () {
 for (const producto of stock) {
-    let {nombre, precio, id} = producto
+    let {nombre, precio, id, img} = producto
     let prod = document.createElement('div')
-    prod.innerHTML=`<div class="card">
-    <h4>${nombre}</h4>
-    <p> $ ${precio}</p>
-    <button id="agregar${id}" class="btnAgregar">Agregar</button>
+    prod.innerHTML=`<div class="card card1 mb-3 sombra" style="max-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+        <img src="../imagenes/${img}" class="img-fluid rounded-start cardImage1" alt="...">
+    </div>
+    <div class="col-md-8 ">
+        <div class="card-body h-100 cardContent">
+            <h5 class="card-title">${nombre}</h5>
+            <h4 class="card-text"> $ ${precio}</h4>
+            <button id="agregar${id}" class="btnAgregar w-80">Agregar</button>
+        </div>
+    </div>
+    </div>
     </div>`
     stockProductos.append(prod) 
 }
@@ -137,22 +181,33 @@ function agregarCarrito(producto){
     }
 
 // mostrar el carrito de compras (use destructuring)
+let precioTot;
 function mostrarCarrito(){
     compras.innerHTML = "";
 for (const producto of carrito) {
-    let {nombre, precio, id, cantidad} = producto
+    let {nombre, precio, id, img, cantidad} = producto
     let prodCarrito = document.createElement('div')
-    prodCarrito.innerHTML=`<div class="card">
-    <h4>${nombre}</h4>
-    <p> $ ${precio*cantidad}</p>
-    <h5>CANTIDAD: ${cantidad}</h5>
-    <button class="btnCarrito btnBorrar" id="btn-borrar${id}">Borrar</button>
+
+    prodCarrito.innerHTML=`<div class="card card1 mb-3 sombra" style="max-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+        <img src="../imagenes/${img}" class="img-fluid rounded-start " alt="...">
+    </div>
+    <div class="col-md-8 ">
+        <div class="card-body h-100 cardContent">
+            <h5 class="card-title">${nombre}</h5>
+            <p class="card-text"> $ ${precio*cantidad}</p>
+            <h5>CANTIDAD: ${cantidad}</h5>
+            <button class="btnCarrito btnBorrar" id="btn-borrar${id}">Borrar</button>
+        </div>
+    </div>
+    </div>
     </div>`
     compras.append(prodCarrito) 
 }
 localStorage.setItem("carrito", JSON.stringify(carrito))
 borrar();
-let precioTot;
+// let precioTot;
     precioTot = carrito.reduce((acc,el)=> acc + ((el.precio)*(el.cantidad)), 0 );
 precioTotal.innerText= "El valor de su compra es de $" + precioTot;
 }
@@ -171,6 +226,13 @@ function borrar(){
 //finalizar
 function fin(){
     finalizar.addEventListener("click",()=>{
+        
+        Swal.fire({
+            icon: 'success',
+            title: 'Compra exitosa!!!',
+            text: 'Su compra finalizo correctamente, esperamos volver a verlo pronto muchas gracias por su visita 😀',
+        })
+
         localStorage.removeItem("carrito");
         for (let index = 0; index < carrito.length; index++) {
             carrito.splice(index,carrito.length);;
@@ -179,6 +241,67 @@ function fin(){
         console.log(carrito);
         mostrarCarrito();
     })
+}
+
+fin();
+
+//forma de pago con evento change
+let pagar;
+let totalPagar;
+let valorCuota;
+formaPago.addEventListener('change', () => {
+    pagar = formaPago.value.toLowerCase()
+    seleccionPago();
+});
+
+//funcion para seleccionar forma de pago
+const seleccionPago = () => {
+    if (pagar === 'debito' || pagar === 'transferencia') {
+        divCuota.classList.add('oculta')
+        finalizar.classList.remove('oculta')
+        totalPagar = precioTot * 0.85;
+        //mensaje libreria pago realizado con exito
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Realizó su pago con exito',
+            color: 'rgb(156, 19, 138)',
+            text: `Usted eligió la forma de pago ${pagar} con un descuento del 15%, su compra total es de $ ${totalPagar} Final`,
+            showConfirmButton: false,
+            timer: 4500
+        })
+        tipoDePago.innerText=`Usted eligió la forma de pago ${pagar} con un descuento del 15%, su compra total es de $ ${totalPagar} Final`;
+    } else {
+        divCuota.classList.remove('oculta')
+        totalPagar = precioTot;
+        //evento change para elegir cuotas
+        cuotas.addEventListener('change', () => {
+            valorCuota = cuotas.value.toLowerCase();
+            console.log(valorCuota);
+            seleccionCuotas();
+            //mensaje libreria pago realizado con exito
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                color: 'rgb(156, 19, 138)',
+                text: `Usted eligió la forma de pago ${pagar} en cantidad de cuotas, ${valorCuota}, monto de cuota a pagar ${parseInt(totalCuota)} Final`,
+                showConfirmButton: false,
+                timer: 5500
+            })
+            tipoDePago.innerText=`Usted eligió la forma de pago ${pagar} en cantidad de cuotas, ${valorCuota} , monto de cuota a pagar $ ${parseInt(totalCuota)} Final`;
+        })
+    }
+}
+
+//funcion para seleccionar cuotas
+const seleccionCuotas = () => {
+    if (valorCuota === 'una') {
+        totalCuota = totalPagar / 1;
+    } else if (valorCuota === 'tres') {
+        totalCuota = (totalPagar * 1.25) / 3;
+    } else {
+        totalCuota = (totalPagar * 1.40) / 6;
+    }
 }
 
 //cancelar
@@ -226,5 +349,4 @@ function cancel(){
 
 mostrarCarrito();
 tarjetasStock();
-fin();
 cancel();
